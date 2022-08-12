99b8c21c71b314530b398aee228a1afc10564297
GabrielSantillo <81308836+gabrielSantillo@users.noreply.github.com>
2022-08-11 20:03:39 -0600

    just changed a detail

--- src/components/TemplateFun.vue
+++ src/components/TemplateFun.vue
@@ -18,31 +18,29 @@
 
         methods: {
             changePost() {
-                this.posts[0].show_post = !this.posts[0].show_post;
+                this.show_post = !this.show_post;
             }
         },
 
         data() {
             return {
-
+                show_post : true,
                 posts: [
                     {
                         blog_content: `Content blog.`,
                         blog_title: `Blog Title`,
-                        blog_img_url: `https://images.pexels.com/photos/39317/chihuahua-dog-puppy-cute-39317.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1`,
-                        show_post : true
+                        blog_img_url: `https://images.pexels.com/photos/39317/chihuahua-dog-puppy-cute-39317.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1`
+                        
                     },
                     {
                         blog_content: `Content blog.`,
                         blog_title: `Blog Title`,
-                        blog_img_url: `https://images.pexels.com/photos/39317/chihuahua-dog-puppy-cute-39317.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1`,
-                        show_post : true
+                        blog_img_url: `https://images.pexels.com/photos/39317/chihuahua-dog-puppy-cute-39317.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1`
                     },
                     {
                         blog_content: `Content blog.`,
                         blog_title: `Blog Title`,
-                        blog_img_url: `https://images.pexels.com/photos/39317/chihuahua-dog-puppy-cute-39317.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1`,
-                        show_post : true
+                        blog_img_url: `https://images.pexels.com/photos/39317/chihuahua-dog-puppy-cute-39317.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1`
                     }
                 
                 ]
