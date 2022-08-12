9cdb6b02604edc61f82552366dc6c054b0bfc173
GabrielSantillo <81308836+gabrielSantillo@users.noreply.github.com>
2022-08-11 18:01:19 -0600

    Finished the warmup

--- src/App.vue
+++ src/App.vue
@@ -1,28 +1,20 @@
 <template>
   <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <template-fun></template-fun>
   </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
-
+import TemplateFun from '@/components/TemplateFun.vue'
 export default {
+  
   name: 'App',
   components: {
-    HelloWorld
+    TemplateFun
   }
 }
 </script>
 
 <style>
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
-}
+
 </style>
---
+++ src/components/TemplateFun.vue
@@ -0,0 +1,23 @@
+<template>
+    <div>
+        <h1>{{ blog_title }}</h1>
+        <p>{{ blog_content }}</p>
+        <img :src="blog_img_url" alt="">
+    </div>
+</template>
+
+<script>
+    export default {
+        data() {
+            return {
+                blog_content: `Content blog.`,
+                blog_title: `Blog Title`,
+                blog_img_url: `https://images.pexels.com/photos/39317/chihuahua-dog-puppy-cute-39317.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1`
+            }
+        },
+    }
+</script>
+
+<style scoped>
+
+</style>
