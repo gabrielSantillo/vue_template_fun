de1167de674b462a1a1ecb8f60f748891300e792
GabrielSantillo <81308836+gabrielSantillo@users.noreply.github.com>
2022-08-11 20:04:14 -0600

    Added a defaut formatter

--- src/components/TemplateFun.vue
+++ src/components/TemplateFun.vue
@@ -1,55 +1,48 @@
 <template>
-    <div>
-        <button @click="changePost">Turn on/off</button>
+  <div>
+    <button @click="changePost">Turn on/off</button>
 
-        <section v-if="posts[0][`show_post`] === true">
-            <div v-for="(post, index) in posts" :key="index">
-            <p>{{ post[`blog_content`] }}</p>
-            <p>{{ post[`blog_title`] }}</p>
-            <img :src="post[`blog_img_url`]" alt="">
-        </div>
-        </section>
-
-    </div>
+    <section v-if="posts[0][`show_post`] === true">
+      <div v-for="(post, index) in posts" :key="index">
+        <p>{{ post[`blog_content`] }}</p>
+        <p>{{ post[`blog_title`] }}</p>
+        <img :src="post[`blog_img_url`]" alt="" />
+      </div>
+    </section>
+  </div>
 </template>
 
 <script>
-    export default {
+export default {
+  methods: {
+    changePost() {
+      this.show_post = !this.show_post;
+    },
+  },
 
-        methods: {
-            changePost() {
-                this.show_post = !this.show_post;
-            }
+  data() {
+    return {
+      show_post: true,
+      posts: [
+        {
+          blog_content: `Content blog.`,
+          blog_title: `Blog Title`,
+          blog_img_url: `https://images.pexels.com/photos/39317/chihuahua-dog-puppy-cute-39317.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1`,
         },
-
-        data() {
-            return {
-                show_post : true,
-                posts: [
-                    {
-                        blog_content: `Content blog.`,
-                        blog_title: `Blog Title`,
-                        blog_img_url: `https://images.pexels.com/photos/39317/chihuahua-dog-puppy-cute-39317.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1`
-                        
-                    },
-                    {
-                        blog_content: `Content blog.`,
-                        blog_title: `Blog Title`,
-                        blog_img_url: `https://images.pexels.com/photos/39317/chihuahua-dog-puppy-cute-39317.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1`
-                    },
-                    {
-                        blog_content: `Content blog.`,
-                        blog_title: `Blog Title`,
-                        blog_img_url: `https://images.pexels.com/photos/39317/chihuahua-dog-puppy-cute-39317.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1`
-                    }
-                
-                ]
-
-            }
+        {
+          blog_content: `Content blog.`,
+          blog_title: `Blog Title`,
+          blog_img_url: `https://images.pexels.com/photos/39317/chihuahua-dog-puppy-cute-39317.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1`,
         },
-    }
+        {
+          blog_content: `Content blog.`,
+          blog_title: `Blog Title`,
+          blog_img_url: `https://images.pexels.com/photos/39317/chihuahua-dog-puppy-cute-39317.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1`,
+        },
+      ],
+    };
+  },
+};
 </script>
 
-<style scoped>
-
-</style>
+<style scoped></style>
